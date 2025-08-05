6768d5e72e33c21344251e5c303d9f7f109fe532
Maria Juliana Saavedra <mariajulianasaavedra@Marias-MacBook-Air.local>
2025-08-05 08:48:23 -0500

    Feat :sparkles: update in python work

--- Dia1/index.js
+++ Dia1/index.js
@@ -1,4 +1,61 @@
-console.log("Hola Mundo");
-console.log("Hola Mundo");
-console.log("Hola Mundo");
-console.log("Hola Mundo");
+// #####################################
+// Menus
+// #####################################
+// Menu principal 
+export function menuPrincipal(){
+    alert(
+        "=======================================\n"+
+        "======= Bienvenido a Campuslands ✨😁 =========\n"+
+        "=======================================\n"+
+        "======= Estas son las opciones a ingresar ==========\n"+
+        "=======================================\n"+
+        " 1. Camper \n"+
+        " 2. Trainer \n"+
+        " 3. Coordinador\n"+
+        "=======================================\n"
+    )
+}
+
+// Menu estudiante 
+export function menuEstudiante(){
+    alert(
+        "===================================\n"+
+        "======= ¿Que desea Hacer? 😃 =====\n"+
+        "===================================\n"+
+        " 1. Inscripción \n"+
+        " 2. Ingresar al Perfil \n"+
+        " 3. Salir de Campus \n"+
+        "==================================\n")
+}
+
+// Menu trainer
+export function menuTrainer(){
+    alert(    
+        "===================================\n"+
+        "= Bienvenido Trainer, identifiquese por favor 😃 =\n"+
+        "===================================\n"+
+        " 1. Pedro Gomez \n"+
+        " 2. Miguel Rodriguez \n"+
+        " 3. Juan Nariño \n"+
+        " 4. Santiago Melo \n"+
+        " 5. Carlos Rueda \n"+
+        " 6. Antonio Vega \n"+
+        " 7. Salir del Programa\n"+
+        "==================================\n")
+}
+
+// Menu coordinador
+export function menuCoordinador(){
+    alert(    
+        "===================================\n"+
+        "= Bienvenido Coordinador, Digite lo que quiere realizar 😃 =\n"+
+        "===================================\n"+
+        " 1. Agregar notas prueba de inscripcion  \n"+
+        " 2. Agregar trainers \n"+
+        " 3. Agregar modulos \n"+
+        " 4. Modulo matriculas \n"+
+        " 5. Eliminar trainers \n"+
+        " 6. Eliminar modulos \n"+
+        " 7. Salir del Programa\n"+
+        "==================================\n")
+}
