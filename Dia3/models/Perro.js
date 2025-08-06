bdc3ec5573cae5cad26bed9eeb8f8fbb0ed40a93
saavedra.mariaju@gmail.com <saavedra.mariaju@gmail.com>
2025-08-06 09:00:46 -0500

    Feat :sparkles: update day 3

---
+++ Dia3/models/Perro.js
@@ -0,0 +1,21 @@
+// ###################
+// P.O.O - Herencia
+// ###################
+
+const Animal = require("./Animal");
+
+class Perro extends Animal{
+    //Sobreescritura de método
+    hablar(){
+        console.log(`${this.nombre} está ladrando.`);
+    }
+}
+/*
+Aquí, Perro hereda de Animal, lo cual
+evita la duplicación de código, facilitando
+el mantenimiento del mismo y la extensión de
+comportamientos con facilidad.
+*/
+
+//exporta el modulo a utilizar
+module.exports=Perro;
