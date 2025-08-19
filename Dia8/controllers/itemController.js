d7049dda9d54074427a6c2b0cf72348ce11738ff
saavedra.mariaju@gmail.com <saavedra.mariaju@gmail.com>
2025-08-19 09:28:12 -0500

    Feat :sparkles: update in model

--- Dia8/controllers/itemController.js
+++ Dia8/controllers/itemController.js
@@ -3,12 +3,19 @@
 // coordinar el modelo 
 
 class ItemController{
-    constructor ({model, view, prompt}){
+    constructor({model,view,prompt}){
         this.model=model;
         this.view=view;
-        this.prompt=prompt;
+        this.prompt = prompt
     }
     crear(){
-        
+        const datos = this.view.pedirDatosCreacion(this.prompt);
+        if(!datos.nombre){
+            this.view.mostrarMensaje("El nombre es obligatorio.");
+            return;
+        }
+        const creado = this.model.crear(datos);
+        this.view.mostrarMensaje(`Creado con ID ${creado.id}.`);
     }
 }
+module.exports={ItemController};
